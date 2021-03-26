3abc1c510fa0f837a43b68131eb0448d3c430914
dnodolf1212 <dnodolf@gmail.com>
2021-03-26 00:55:05 -0700

    hooked mongodb up with keystone - have a running db now

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,3 +1,32 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 
-const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+const databaseURL = 
+process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config ({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    //data seeding will go here
+  },
+  lists: createSchema({
+    //schema items here
+  }),
+  ui: {
+    // roles will go here
+    isAccessAllowed: () => true,
+  },
+  //add session values here
+});
