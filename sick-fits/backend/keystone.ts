e5c021a3d74b98eb41f47f012cb4bd3080726ac6
dnodolf1212 <dnodolf@gmail.com>
2021-05-18 17:55:09 -0700

    addToCart mutation for backend. Use addToCart in index.ts as resolver mutation. Track quantity of existing and new items added to cart.

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,16 +1,20 @@
-import { CartItem } from './schemas/CartItem';
 import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { CartItem } from './schemas/CartItem';
+import { extendGraphqlSchema } from './mutations';
 
-const databaseURL = 
-process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360,
@@ -23,50 +27,51 @@ const { withAuth } = createAuth({
   secretField: 'password',
   initFirstItem: {
     fields: ['name', 'email', 'password'],
-    //add initial roles
+    // add initial roles
   },
   passwordResetLink: {
     async sendToken(args) {
-      //send the email 
+      // send the email
       await sendPasswordResetEmail(args.token, args.identity);
     },
   },
 });
 
-export default withAuth(config ({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
     },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    async onConnect(keystone) {
-      console.log('connected bro');
-      if (process.argv.includes('--seed-data')) {
-        await insertSeedData(keystone);
-      }
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('connected bro');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
-  },
-  lists: createSchema({
-    //show for UI for people who pass auth test
-    User,
-    Product,
-    ProductImage,
-    CartItem
-  }),
-  ui: {
-    
-    isAccessAllowed: ({session}) => {
-      //console.log(session);
-      return !!session?.data; 
-    }
-  },
-  session: withItemData(statelessSessions(sessionConfig), {
-    //this is a GraphQL query
-    User: 'id name email'
+    lists: createSchema({
+      // show for UI for people who pass auth test
+      User,
+      Product,
+      ProductImage,
+      CartItem,
+    }),
+    extendGraphqlSchema,
+    ui: {
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // this is a GraphQL query
+      User: 'id name email',
     }),
   })
 );
--- sick-fits/backend/schemas/CartItem.ts
+++ sick-fits/backend/schemas/CartItem.ts
@@ -1,21 +1,19 @@
-import { list } from "@keystone-next/keystone/schema";
-import { text, select, integer, relationship } from "@keystone-next/fields";
-
+import { list } from '@keystone-next/keystone/schema';
+import { text, select, integer, relationship } from '@keystone-next/fields';
 
 export const CartItem = list({
   ui: {
     listView: {
-      initialColumns: ['product', 'quantity', 'user']
-    } 
+      initialColumns: ['product', 'quantity', 'user'],
+    },
   },
   fields: {
-    //Custom Label coming soon
+    // Custom Label coming soon
     quantity: integer({
       defaultValue: 1,
       isRequired: true,
     }),
-    product: relationship({ref: 'Product'}),
-    user: relationship({ ref: 'User.cart'}),
+    product: relationship({ ref: 'Product' }),
+    user: relationship({ ref: 'User.cart' }),
   },
-  
-})
+});
